f921bb54dba10468f1140a7e48d0fc4d46eb2caa
SGCEntertainment <nikcxprojects@gmail.com>
2023-04-08 21:54:00 +0300

    update

---
+++ Assets/Third/Scripts/OneSignalInitializer.cs
@@ -0,0 +1,15 @@
+using OneSignalSDK;
+using UnityEngine;
+
+public class OneSignalInitializer : MonoBehaviour
+{
+
+    private void Start()
+    {
+        // Enable lines below to debug issues with OneSignal
+        OneSignal.Default.LogLevel = LogLevel.Info;
+        OneSignal.Default.AlertLevel = LogLevel.Fatal;
+
+        OneSignal.Default.Initialize("ead9cffa-d96d-4126-8665-3c72931056ae");
+    }
+}
